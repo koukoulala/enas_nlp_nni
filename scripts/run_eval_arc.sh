1b5017e1786315485b6e06286bf4aee8bf63fbab
koukou <714366968@qq.com>
2019-11-02 17:26:27 +0800

    k

--- scripts/run_eval_arc.sh
+++ scripts/run_eval_arc.sh
@@ -2,7 +2,7 @@
 set -e
 export PYTHONPATH="$(pwd)"
 
-python3 ./nni_child_model/entry.py \
+python3 ./nni_child_model/eval_arc.py \
   --train_ratio=1.0 \
   --valid_ratio=1.0 \
   --embedding_model="glove" \
