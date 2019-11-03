3c59e6a719e731022001c2102a77e5b5981392b9
koukou <714366968@qq.com>
2019-11-03 10:54:42 +0800

    k

---
+++ find_best.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import pandas as pd
+import numpy as np
+
+nni_id = sys.argv[1]
+file_path = '~/nni/experiments/' + nni_id + 'log/RL_controller.log'
+os.environ['file_path'] = str(file_path)
+os.system("grep 'reward' $file_path > tmp.txt")
+
+data = pd.read_csv("tmp.txt", header=None, sep='\t')
+data.columns = ['arc', 'arc_num', 'reward', 'reward_num']
+max_reward = data.ix[data['reward_num'].idxmax()]
+arc_str = max_reward['arc_num']
+
+fw = open("./scripts/arcs.sh", 'w')
+fw.write("#!/bin/bash\n")
+
+arc = [int(i) for i in arc_str.split(' ')]
+start = 0
+for layer_id in range(12):
+    end = start + 1 + layer_id
+    end += 1
+    out_str = "fixed_arc=\"$fixed_arc {0}\"".format(np.reshape(arc[start: end], [-1]))
+    out_str = out_str.replace("[", "").replace("]", "")
+    print(out_str)
+    fw.write(out_str+'\n')
+    start = end
+
+fw.close()
--- find_best.sh
+++
@@ -1,3 +0,0 @@
-
-file_path = $1
-grep "reward" $file_path > tmp.txt
